e39844b3c8380b42270becae78d537cc230fa908
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-02-07 18:58:12 +0530

    vector done

--- STL libraries/vector.cpp
+++ STL libraries/vector.cpp
@@ -3,5 +3,36 @@
 using namespace std;
 
 int main(){
+vector<int>v;
+cout<<"Capacity : "<<v.capacity()<<endl;
+
+v.push_back(1);
+cout<<"Capacity : "<<v.capacity()<<endl;
+
+v.push_back(2);
+cout<<"Capacity : "<<v.capacity()<<endl;
+
+v.push_back(3);
+cout<<"Capacity : "<<v.capacity()<<endl;
+cout<<"Size : "<<v.size()<<endl;
+
+cout<<" 2nd position "<<v.at(2)<<endl;
+
+cout<<" front : "<<v.front()<<endl;
+cout<<"back : "<<v.back()<< endl;
+
+
+cout<<"before pop "<<endl;
+for(int i:v){
+    cout<<i<<" ";
+}cout<<endl;
+
+
+v.pop_back();
+
+cout<<"after pop "<<endl;
+for(int i:v){
+    cout<<i<<" ";
+}cout<<endl;
 
 }
