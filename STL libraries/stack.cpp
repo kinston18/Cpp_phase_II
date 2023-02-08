e63e0db821a8ccb73cf0d78d07aff6cbbc29bed1
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-02-09 02:12:41 +0530

    stack STL

---
+++ STL libraries/stack.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+stack<string>s;
+
+s.push("Prajjwal");
+s.push("Kush");
+s.push("Harsh");
+
+
+cout<<"Top element : "<<s.top()<<endl;
+
+
+}
