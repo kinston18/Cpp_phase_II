05649da54dcc77b915a4f234d97f8d707ac2c672
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-02-08 23:58:09 +0530

    deque

--- STL libraries/deque.cpp
+++ STL libraries/deque.cpp
@@ -7,4 +7,15 @@ int main()
     deque<int>d;
     d.push_front(3);
     d.push_back(2);
+    d.push_front(5);
+
+for(int i:d)
+{
+    cout<<i<<" ";
+    }
+d.pop_front();
+cout<<endl;
+for(int i:d){
+    cout<<i<<" ";
+}
 } 
