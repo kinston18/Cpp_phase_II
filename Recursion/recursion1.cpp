8e66fa5eb95d59a6021b62deac5037fe9c87701c
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-07-07 16:59:30 +0530

    recursion day 1

---
+++ Recursion/recursion1.cpp
@@ -0,0 +1,29 @@
+//factorial of a no.
+
+
+#include<iostream>
+using namespace std;
+
+
+int factorial(int n){
+
+    //base case
+    if(n==0)
+    return 1;
+
+    int chhoti = factorial(n-1);
+    int badi = n * chhoti;
+
+    return badi;
+
+}
+int main(){
+
+int n;
+cin>>n;
+
+int ans= factorial(n);
+
+cout<<ans<<endl;
+    return 0;
+}
