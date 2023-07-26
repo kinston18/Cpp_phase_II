62aff0ec296dc6b59469ab958e84b30402de9fa0
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-07-27 01:23:45 +0530

    good night

---
+++ Recursion/walking.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void reachHome(int src, int dest){
+
+    //base case
+    if(src==dest)
+    cout<<"pahunch gya"<<endl;
+    return ;
+
+    //recursive call
+    reachHome(src+1, )
+}
+
+
+
+int main(){
+
+int dest = 10;
+int src = 1;
+
+reachHome(src,dest);
+
+    return 0;
+}
