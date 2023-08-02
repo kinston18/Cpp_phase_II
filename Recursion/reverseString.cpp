13f8d95821d2d22d41be16c5e6843f559921ed3c
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-08-02 23:53:37 +0530

    reverse string using recursion

---
+++ Recursion/reverseString.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void reverse(string str, int i, int j){
+
+    //base case
+    if(i>j)
+    return ;
+
+    swap(str [i], str[j]);
+    i++;
+}
+int main(){
+
+string name = "prajjwal";
+
+    return 0;
+}
