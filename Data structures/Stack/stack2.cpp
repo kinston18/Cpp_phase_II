64abced969b5007c1dc9505ec5b187e68bcbfcdc
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-08-04 23:23:25 +0530

    stack cooking

--- Data structures/Stack/stack2.cpp
+++ Data structures/Stack/stack2.cpp
@@ -5,9 +5,17 @@ using namespace std;
 int main(){
     stack <int> stack;
     stack.push(24);
+    stack.push(25);
+    stack.push(26);
+    stack.push(27);
+    int num= 0;
+    stack.push(num);
+    stack.pop();
     stack.pop();
 
 
+//codition
+while
     cout<<stack.top()<<endl;
 
 }
