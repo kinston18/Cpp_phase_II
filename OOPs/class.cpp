c74ab6e98c12ad57d8ca09aaba3c76305b51f704
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-08-06 01:46:50 +0530

    pvt classes

--- OOPs/class.cpp
+++ OOPs/class.cpp
@@ -5,16 +5,33 @@ using namespace std;
 class Employee{
 
 private:
-int a, b,c;
+int a,b,c;
 
 public:
 int d,e;
+void setData(int a1, int b1, int c1);
+void getData(){
+    cout<<"The value of a is "<<a<<endl;
+    cout<<"The value of a is "<<b<<endl;
+    cout<<"The value of a is "<<c<<endl;
+    cout<<"The value of a is "<<d<<endl;
+    cout<<"The value of a is "<<e<<endl;
+}
 
 };
 
+void Employee :: setData(int a1, int b1, int c1){
+a=a1;
+b=b1;
+c=c1;
+}
 
 int main(){
-
+Employee harry;
+harry.d=23;
+harry.e=32;
+harry.setData(1,2,4);
+harry.getData();
 
 
     return 0;
