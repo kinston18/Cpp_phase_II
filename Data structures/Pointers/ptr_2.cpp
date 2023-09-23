50b16c02fc47f31bf3623bff148a7cf62d3a3157
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-09-24 02:40:40 +0530

    gunnite

--- Data structures/Pointers/ptr_2.cpp
+++ Data structures/Pointers/ptr_2.cpp
@@ -3,8 +3,35 @@ using namespace std;
 
 int main(){
     //pointer to int is created, and pointing to some garbage address
-int *p = 0;
+//int *p = 0;
 
-cout<< *p <<endl;
-    
+//cout<< *p <<endl;
+int i =5;
+
+int *q = &i;
+cout<<q<<endl;
+cout<<*q<<endl;
+
+int *p = 0;
+p=&i;
+
+cout<< p <<endl;
+cout<<*p<<endl;
+
+cout<<endl;
+
+int num=5;
+int a=num;
+a++;
+
+
+cout<<num<<endl;
+
+int *t =&num;
+cout<<"before : " <<num<<endl;
+(*t)++;
+cout<<"after : " <<num<<endl;
+
+return 0;
+
 }
