f2e297ad4d5df317ee891893e41919511a6452e5
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-10-14 14:08:55 +0530

    classes and obj

---
+++ OOPs/intro_class.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class Hero{
+
+    //properties
+};
+
+int main(){
+
+    //object creation
+    Hero h1;
+
+    cout<<"size : "<<sizeof(h1)<<endl;
+
+    return 0;
+}
