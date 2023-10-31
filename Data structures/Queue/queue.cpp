19e44e900d73faafb03bd8d28949f1d04953d3b4
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-10-31 23:11:46 +0530

    queue1

--- Data structures/Queue/queue.cpp
+++ Data structures/Queue/queue.cpp
@@ -10,13 +10,14 @@ int main()
     
     q.push(11);
     cout<<"front of q is: " <<q.front()<<endl;
+
     q.push(15);
     cout<<"front of q is: " <<q.front()<<endl;
     
     q.push(13);
     cout<<"front of q is: " <<q.front()<<endl;
     
-
+//size
     cout<<"size of q is: " <<q.size()<<endl;
 
     q.pop();
