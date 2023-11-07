887baaa2aa84da43c2578346edcc7bd17a2be59a
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-11-07 10:29:20 +0530

    concatenation

--- Maths/rat.cpp
+++ Maths/rat.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 using namespace std;
 
-
-
 int main(){
 
+int s[10],v;
+int i,n,r=s[0]-48;
+for(i=1;i<n;i+=2){
+switch (s[i])
+{
+case 'A':
+v=s[i+1]-48;
+    r=r&v;
+
+case 'B':
+v=s[i+1]-48;
+    r=r|v;
+
+case 'C':
+v=s[i+1]-48;
+    r=r^v;
+
+}
+}
+
+return r;
 }
