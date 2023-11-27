39165a71001ed27965d1fcfcd945871128eb93e9
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-11-28 02:13:22 +0530

    chklehlh

---
+++ STL libraries/test_rev.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+
+int reverse(int n){
+vector<int>v;
+
+while(n>0){
+    v.push_back(n%10);
+    n=n/10;
+}
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<reverse(n);
+
+    return 0;
+}
--- STL libraries/vector.cpp
+++ STL libraries/vector.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 using namespace std;
 
+
 int main(){
 vector<int>v;
 cout<<"Capacity : "<<v.capacity()<<endl;
