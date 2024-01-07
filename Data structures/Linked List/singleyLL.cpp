97867afad5d293175b80ca509de90bf4be226442
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2024-01-07 20:58:11 +0530

    singly LL

--- Data structures/Linked List/singleyLL.cpp
+++ Data structures/Linked List/singleyLL.cpp
@@ -3,75 +3,23 @@ using namespace std;
 
 class Node{
 
-    public:
-    int data;
-    Node* next;
+public:
+int data;
+Node* next;
 
-    
 //constructor
-    Node(int data){
-        this -> data = data;
-        this -> next = NULL;
+Node(int data){
+    this -> data = data;
+    this -> next = NULL;
+}
 
-        
-    }
-    
 };
 
-void insertAtTail(Node* &tail, int d){
+int main(){
 
-//new node creation
-Node* temp = new Node(d);
-tail -> next = temp;
-tail = tail -> next;
+Node* node1 = new Node(10);
+cout<<node1 -> data<<endl;
+cout<<node1 -> next<<endl;
 
-}
-
-void insertAtHead(Node* &head, int d){
-
-    //new node creation
-    Node* temp = new Node(d);
-    temp -> next = head;
-    head = temp;
-}
-
-void print(Node* &head){
-Node *temp = head;
-
-while(temp != NULL){
-    cout<< temp -> data <<endl;
-    temp= temp -> next;
-}
-cout<<endl;
-}
-
-void insertAtPosition(int position, int d){
-
-Node* temp = new Node(d);
-int cnt = 1;
-}
-
-int main() 
-{
-    //created a new node
-    Node* node1 = new Node(10);
-  //cout<< node1 -> data <<endl;
-    //cout<< node1 -> next <<endl;
-
-    //head pointed to node1
-    Node* head = node1;
-    Node* tail = node1;
-    print(tail);
-
-    insertAtTail(tail, 12);
-    print(tail);
-
-    insertAtTail(tail, 15);
-    print(tail);
-
-    insertAtTail(tail, 17);
-    print(tail);
-
-    insertAtTail(tail, 22);
-    print(tail);
+    return 0;
 }
