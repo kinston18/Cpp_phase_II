4af1e7280e056313c6eeb1a71eb1fd86d3da9cb0
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2024-04-16 02:04:41 +0530

    tail recursion

--- Recursion/Print_N-1.cpp
+++ Recursion/Print_N-1.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 
+void print(int i,int n){
+    //base case
+    if(i<1)
+    return;
+
+    cout<<i<<endl;
+
+    print(i-1,n);
+
+}
+
 int main(){
-    
+    int n;
+    cin>>n;
+    cout<<endl;
+    print(n,n);
+    return 0;
 }
