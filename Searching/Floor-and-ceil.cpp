ec8835cd8b2dacef2ea578d2ffbb319c675df702
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2024-06-04 10:24:52 +0530

    first and last occurrence in an array

--- Searching/Floor-and-ceil.cpp
+++ Searching/Floor-and-ceil.cpp
@@ -50,6 +50,6 @@ int main() {
 	int n = 6, x = 5;
 	pair<int, int> ans = getFloorAndCeil(arr, n, x);
 	cout << "The floor and ceil are: " << ans.first
-	     << " " << ans.second << endl;
+	    << " " << ans.second << endl;
 	return 0;
 }
